16efa81c05975a8fda5252990151a52cbce6a39c
VidalYT <vidalgames12345@gmail.com>
2026-02-10 19:39:23 -0800

    Documente los numeros y operadores matematicos de Python

--- sesion_01.py
+++ sesion_01.py
@@ -12,3 +12,24 @@ print("===== Operadores Matematicos =====")
 # /
 # **
 # % Modulo
+print(int(2**3))
+print(int(4**8))
+print(float(10%3))
+print(float(25%4))
+print(float(16%2))
+print(float(10 / 3))
+
+ventas = 199999
+print("Nuestras ventas fueron: ", ventas)
+
+is_active = True
+print(bool(is_active))
+
+game_over = False
+print(game_over)
+
+edad = 18
+if (edad >= 18):
+    print("Si puedo entrar a el Bar!")
+else:
+    print("No puedo entrar a el Bar!")
