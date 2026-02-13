8d9ece07e164383ba1d8030e09cb167352612901
VidalYT <vidalgames12345@gmail.com>
2026-02-12 18:59:52 -0800

    Documente la elaboracion de una cuenta de acceso en Python

--- sesion_02.py
+++ sesion_02.py
@@ -7,3 +7,14 @@ mi_lista = [1, 3.1416, "Christian", mi_tupla]
 print("El primer elemento de mi lista: ", mi_lista[0])
 print("El primer elemento de mi lista: ", mi_lista[3])
 print("El primer elemento de mi lista: ", mi_lista[2])
+
+# Diccionarios
+mi_diccionario = {
+    "mi_lista": mi_lista,
+    "nombre": "christian",
+    "Pi": 3.1416,
+    "Tel": "664-2334455"
+}
+print("Llave para accesar a mi_lista", mi_diccionario["mi_lista"])
+print("Llave para accesar a Pi", mi_diccionario["Pi"])
+print("Llave para accesar a tel", mi_diccionario["Tel"])
